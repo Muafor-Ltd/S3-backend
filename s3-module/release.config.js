4a97f31ccf93082ddf6ce1d53fc6cae94bac303b
Philmuafor <134835913+Philmuafor@users.noreply.github.com>
2023-10-04 00:00:35 +0100

    fix: semantic release Contributing-to: https://kennedymakhanu1.atlassian.net/browse/<JIRA-TICKET> Problem Solution Note

--- s3-module/release.config.js
+++ s3-module/release.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-    branches: "main",
+    branches: ["main"],
     repositoryUrl: "https://github.com/Muafor-Ltd/S3-backend.git",
-    plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/git',
-      '@semantic-release/github',
-      '@semantic-release/npm',
+    "plugins": [
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+      "@semantic-release/changelog",
+      "@semantic-release/npm",
+      "@semantic-release/github"
     ]
- }
+  }
+
+
+
+
